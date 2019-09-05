4305e9a02411d354617d4861b0beb6526804c663
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 01:17:24 -0700

    Annotating, fixing issues with packages

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> (EnumerableType<ValueType> => KeyType) => (EnumerableType<ValueType>) => MapType<KeyType, EnumerableType<ValueType>>
+> MapperFunctionType<A, B> => ListType<A> => Map<B, ListType<A>>
 
 Creates a record tree where the key is a computation on the value and the value is a list of the values that match with that computation.
 
--- index.ts
+++ index.ts
@@ -3,9 +3,29 @@ import reduceValues from "@unction/reducevalues";
 import fresh from "@unction/fresh";
 import of from "@unction/of";
 import get from "@unction/get";
+import {ListType} from "./types";
+import {MapperFunctionType} from "./types";
 
-export default function groupBy (unction) {
-  return function groupByUnction (list) {
-    return reduceValues((accumulated) => (value) => mergeRight(accumulated)(of(unction(value))(mergeRight(get(unction(value))(accumulated) || fresh(list))(of(null)(value)(fresh(list))))(accumulated)))(fresh(new Map()))(list);
+export default function groupBy<A, B> (unction: MapperFunctionType<A, B>) {
+  return function groupByUnction (list: ListType<A>): Map<B, ListType<A>> {
+    return reduceValues(
+      (accumulated: Map<B, ListType<A>>) => (value: A) => mergeRight(
+        accumulated
+      )(
+        of(
+          unction(value)
+        )(
+          mergeRight(
+            get(unction(value))(accumulated) || fresh(list)
+          )(
+            of(null)(value)(fresh(list))
+          )
+        )(accumulated)
+      )
+    )(
+      fresh(new Map())
+    )(
+      list
+    );
   };
 }

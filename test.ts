19dc1e11733e4e24174f1ff9a26052aec45052be
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:46 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,108 @@
+
+import get from "@unction/get";
+
+import groupBy from "./index.ts";
+
+test("Single Object Array", () => {
+  expect(groupBy(
+    get("category")
+  )(
+    [
+      {
+        name: "Hotdog",
+        category: "sandwich",
+      },
+    ]
+  )).toEqual(new Map([
+    ["sandwich", [
+      {
+        name: "Hotdog",
+        category: "sandwich",
+      },
+    ]],
+  ]));
+});
+
+test("Multiple Object Array", () => {
+  expect(groupBy(
+    get("category")
+  )(
+    [
+      {
+        name: "Hotdog",
+        category: "sandwich",
+      },
+      {
+        name: "Club Sandwich",
+        category: "sandwich",
+      },
+    ]
+  )).toEqual(new Map([
+    ["sandwich", [
+      {
+        name: "Hotdog",
+        category: "sandwich",
+      },
+      {
+        name: "Club Sandwich",
+        category: "sandwich",
+      },
+    ]],
+  ]));
+});
+
+// Tap is broken right now :( https://github.com/tapjs/tsame/issues/1
+test("Multiple Map Set", () => {
+  expect(groupBy(
+    get("category")
+  )(
+    new Set([
+      new Map([
+        [
+          "name",
+          "Hotdog",
+        ],
+        [
+          "category",
+          "sandwich",
+        ],
+      ]),
+      new Map([
+        [
+          "name",
+          "Club Sandwich",
+        ],
+        [
+          "category",
+          "sandwich",
+        ],
+      ]),
+    ])
+  )).toEqual(new Map([
+    [
+      "sandwich",
+      new Set([
+        new Map([
+          [
+            "name",
+            "Hotdog",
+          ],
+          [
+            "category",
+            "sandwich",
+          ],
+        ]),
+        new Map([
+          [
+            "name",
+            "Club Sandwich",
+          ],
+          [
+            "category",
+            "sandwich",
+          ],
+        ]),
+      ]),
+    ],
+  ]));
+});
